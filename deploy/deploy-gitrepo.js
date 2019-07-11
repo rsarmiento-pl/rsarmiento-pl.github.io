605bf013ee2b1e91497024ae43d807a97e9bed98
Raymond Sarmiento <rsarmiento@macquariecloudservices.com>
2019-07-11 11:25:33 +0800

    Initial commit

---
+++ deploy/deploy-gitrepo.js
@@ -0,0 +1,44 @@
+// Simple-git without promise
+const simpleGit = require('simple-git')();
+// Shelljs package for running shell tasks optional
+const shellJs = require('shelljs');
+// Simple Git with Promise for handling success and failure
+const simpleGitPromise = require('simple-git/promise')();
+
+// change current directory to repo directory in local
+shellJs.cd('path/to/repo/folder');
+// Repo name
+const repo = 'dummy';  //Repo name
+// User name and password of your GitHub
+const userName = 'rsarmiento-pl';
+const password = 'password';
+// Set up GitHub url like this so no manual entry of user pass needed
+const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${rsarmiento-pl.github.io}`;
+// add local git config like username and email
+simpleGit.addConfig('user.email','rsarmiento@peerlancers.com');
+simpleGit.addConfig('user.name','Raymond Sarmiento');
+// Add remote repo url as origin to repo
+simpleGitPromise.addRemote('origin',gitHubUrl);
+// Add all files for commit
+  simpleGitPromise.add('.')
+    .then(
+       (addSuccess) => {
+          console.log(addSuccess);
+       }, (failedAdd) => {
+          console.log('adding files failed');
+    });
+// Commit files as Initial Commit
+ simpleGitPromise.commit('Intial commit by simplegit')
+   .then(
+      (successCommit) => {
+        console.log(successCommit);
+     }, (failed) => {
+        console.log('failed commmit');
+ });
+// Finally push to online repository
+ simpleGitPromise.push('origin','master')
+    .then((success) => {
+       console.log('repo successfully pushed');
+    },(failed)=> {
+       console.log('repo push failed');
+ });
